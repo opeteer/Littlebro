3a041b8a66125e08176781e342a6f4ea46dc65da
Gerardo M Ardianta <gmayella245@gmail.com>
2026-09-23 12:53:50 +0700

    fixbug

--- backend/core/config.py
+++ backend/core/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     REDIS_URL: str = os.getenv("REDIS_URL", "redis://localhost:6341/0")
     FRONTEND_CORS_ORIGINS: str = os.getenv("FRONTEND_CORS_ORIGINS", "http://localhost:3041")
+    OPENSKY_USERNAME: str = os.getenv("OPENSKY_USERNAME", "")
+    OPENSKY_PASSWORD: str = os.getenv("OPENSKY_PASSWORD", "")
     NASA_FIRMS_MAP_KEY: str = os.getenv("NASA_FIRMS_MAP_KEY", "")
     TELEGRAM_API_ID: str = os.getenv("TELEGRAM_API_ID", "")
     TELEGRAM_API_HASH: str = os.getenv("TELEGRAM_API_HASH", "")
